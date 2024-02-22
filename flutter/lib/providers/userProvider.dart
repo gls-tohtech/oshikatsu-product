fcd38a5b3b60fa6cbfc6ba2d9a50a3ce1f7fe353
Rerurate514 <rerurateyuto@gmail.com>
2024-02-23 01:10:41 +0900

    userProviderを単一の対象に変更

--- flutter/lib/providers/userProvider.dart
+++ flutter/lib/providers/userProvider.dart
@@ -7,12 +7,12 @@ import 'package:oshikatsu_product/models/users/Users.dart';
 final FirebaseFirestore firesotre = FirebaseFirestore.instance;
 final usersRef = firesotre.collection(USERS_TABLE_COLLECTION_NAME);
 
-final userStreamProvider = StreamProvider.autoDispose((ref){
-  final usersSnapshots = usersRef.snapshots();
-  final users = 
-    usersSnapshots.map((querySnapshot) => querySnapshot.docs.map((doc){
-      return UserProfile.fromMap(doc.data());
-    }).toList());
+final userStreamProvider = StreamProvider.autoDispose.family((ref, String userId){
+  final usersSnapshot = usersRef.doc(userId).snapshots();
+  final user = 
+    usersSnapshot.map((snapshot) => 
+      UserProfile.fromMap(snapshot.data() ?? {})
+    );
 
-  return users;
+  return user;
 });
