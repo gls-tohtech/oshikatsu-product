a99365595f7d32d1d03a2a57d951d666b11c7db5
Rerurate514 <rerurateyuto@gmail.com>
2024-02-25 08:22:17 +0900

    コンストラクタを不変に変更し、dbProcessedMapへのアクセサを削除

--- flutter/lib/models/users/UserProfile.dart
+++ flutter/lib/models/users/UserProfile.dart
@@ -1,55 +1,40 @@
 class UserProfile{
-  late final String _name;
-  late final int _birthdayYear;
-  late final int _birthdayMonth;
-  late final String _gender;
-  late final String _iconImageUrl;
-  late final String _biography;
+  final String name;
+  final int birthdayYear;
+  final int birthdayMonth;
+  final String gender;
+  final String iconImageUrl;
+  final String biography;
 
-  late final Map<String, dynamic> _dbProcessedMap;
-  Map<String, dynamic> get dbProcessedMap => _dbProcessedMap; 
-
-  String get name => _name;
-  int get birthdayYear => _birthdayYear;
-  int get birthdayMonth => _birthdayMonth;
-  String get gender => _gender;
-  String get iconImageUrl => _iconImageUrl;
-  String get biography => _biography;
-
-  UserProfile({
-    required String nameArg, 
-    required int birthdayYearArg,
-    required int birthdayMonthArg,
-    required String genderArg, 
-    required String iconImageUrlArg,
-    required String biography,
-  }){
-    _name = nameArg;
-    _birthdayYear = birthdayYearArg;
-    _birthdayMonth = birthdayMonthArg;
-    _gender = genderArg;
-    _iconImageUrl = iconImageUrlArg;
-    _biography = biography;
-
-    _dbProcessedMap = {
-      UserTableColumn.NAME.name: _name, 
-      UserTableColumn.BIRTHDAY_YEAR.name: _birthdayYear,
-      UserTableColumn.BIRTHDAY_MONTH.name: _birthdayMonth,
-      UserTableColumn.GENDER.name: _gender,
-      UserTableColumn.ICON_IMAGE_URL.name: _iconImageUrl,
-      UserTableColumn.BIOGRAPHY.name: _biography,
-    };
-  }
+  const UserProfile({
+    required this.name, 
+    required this.birthdayYear,
+    required this.birthdayMonth,
+    required this.gender, 
+    required this.iconImageUrl,
+    required this.biography,
+  });
 
   factory UserProfile.fromMap(Map<String, dynamic> mapArg){
     return UserProfile(
-        nameArg: mapArg[UserTableColumn.NAME.name],
-        birthdayYearArg: mapArg[UserTableColumn.BIRTHDAY_YEAR.name],
-        birthdayMonthArg: mapArg[UserTableColumn.BIRTHDAY_MONTH.name], 
-        genderArg: mapArg[UserTableColumn.GENDER.name],
-        iconImageUrlArg: mapArg[UserTableColumn.ICON_IMAGE_URL.name],
+        name: mapArg[UserTableColumn.NAME.name],
+        birthdayYear: mapArg[UserTableColumn.BIRTHDAY_YEAR.name],
+        birthdayMonth: mapArg[UserTableColumn.BIRTHDAY_MONTH.name], 
+        gender: mapArg[UserTableColumn.GENDER.name],
+        iconImageUrl: mapArg[UserTableColumn.ICON_IMAGE_URL.name],
         biography: mapArg[UserTableColumn.BIOGRAPHY.name],
       );
+  }
+
+  Map<String, dynamic> getDbProcessedMap(){
+    return {
+      UserTableColumn.NAME.name: name, 
+      UserTableColumn.BIRTHDAY_YEAR.name: birthdayYear,
+      UserTableColumn.BIRTHDAY_MONTH.name: birthdayMonth,
+      UserTableColumn.GENDER.name: gender,
+      UserTableColumn.ICON_IMAGE_URL.name: iconImageUrl,
+      UserTableColumn.BIOGRAPHY.name: biography,
+    };
   }
 }
 
