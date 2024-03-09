30ecb34ce6fe882ef364b9fdc98c7aa6106f9cd5
Rerurate514 <rerurateyuto@gmail.com>
2024-03-09 19:31:35 +0900

    エラーハンドリングの修正

--- flutter/lib/screens/fragments/projectDetailFragment/projectDetailImgCom.dart
+++ flutter/lib/screens/fragments/projectDetailFragment/projectDetailImgCom.dart
@@ -15,6 +15,19 @@ class ProjectDetailImgComponent extends StatelessWidget{
           ? Image.network(
             _imageUrl,
             width: size.width,
+            errorBuilder: (c, o, s) {
+              print("error = $c $o $s");
+              return SizedBox(
+                width: size.width,
+                height: size.height * 0.2,
+                child: const Center(
+                  child: Icon(
+                    Icons.error,
+                    color: Colors.red,
+                  ),
+                ),
+              );
+            },
           )
           : Container(
             decoration: BoxDecoration(
