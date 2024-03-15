9f01243d37428c618b547795fe35fd11e499c09a
Rerurate514 <rerurateyuto@gmail.com>
2024-03-16 00:22:53 +0900

    refProviderの追加

--- flutter/lib/providers/projectProvider.dart
+++ flutter/lib/providers/projectProvider.dart
@@ -3,10 +3,10 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:oshikatsu_product/models/projects/project.dart';
 
 final FirebaseFirestore firestore = FirebaseFirestore.instance;
-final projectsRef = firestore.collection(PROJECTS_TABLE_COLLECTION_NAME);
+final projectsCollection = firestore.collection(PROJECTS_TABLE_COLLECTION_NAME);
 
 final projectStreamProvider = StreamProvider.autoDispose.family((ref, String projectId){
-  final projectsSnapshot = projectsRef.doc(projectId).snapshots();
+  final projectsSnapshot = projectsCollection.doc(projectId).snapshots();
   final project = 
     projectsSnapshot.map((snapshot) => 
       Project.fromMap(snapshot.data() ?? {})
@@ -16,11 +16,21 @@ final projectStreamProvider = StreamProvider.autoDispose.family((ref, String pro
 });
 
 final projectsStreamProvider = StreamProvider.autoDispose((ref){
-  final projectsSnapshot = projectsRef.snapshots();
+  final projectsSnapshot = projectsCollection.snapshots();
   final projects = 
     projectsSnapshot.map((querySnapshot) => querySnapshot.docs.map((doc){
       return Project.fromMap(doc.data());
     }).toList());
 
   return projects;
+});
+
+final projectStreamProviderFromRef = StreamProvider.autoDispose.family((ref, DocumentReference projectRef){
+  final projectsSnapshot = projectRef.snapshots();
+  final project = 
+    projectsSnapshot.map((snapshot) => 
+      Project.fromMap(snapshot.data() as Map<String, dynamic> ?? {})
+    );
+
+  return project;
 });
