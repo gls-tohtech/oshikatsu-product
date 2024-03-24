d5085154acb3b45d8ed6d89cbbeec6586bdaee2e
Rerurate514 <rerurateyuto@gmail.com>
2024-03-24 23:14:50 +0900

    バリデートメソッドの修正

--- flutter/lib/utils/validate.dart
+++ flutter/lib/utils/validate.dart
@@ -1,66 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:oshikatsu_product/utils/showDialog.dart';
 
-void validate(BuildContext context, Map<ValidateType, dynamic> targetValueMap){
-  targetValueMap.forEach((ValidateType key, dynamic value) {
-    if(isIncludeKeys(value, key.allowList)){
-      showWarnDialog(
-        context, 
-        "${key.title}が無効な値、または入力されていない可能性があります。"
-      );
-    }
-  });
-}
+bool isNotIncludeInvalidValue(BuildContext context, Validate target){
+  List<String> invalidCharList = findInvalidValue(target.value.toString(), target.validateType.allowListStr);
+  if(isIncludeKeys(target.value.toString(), target.validateType.allowListStr)){
+    showWarnDialog(
+      context, 
+      invalidCharList.isEmpty
+        ? "${target.validateType.title}が入力されていません。"
+        : "無効な値 $invalidCharList を使用しています。\n使用可能な値は、\n${target.validateType.allowRange}\nです。"
+    );
 
-enum ValidateType{
-  EMAIL,
-  PASSWORD,
-  NUM,
-  COMMON_STRING
-}
-
-extension ValidateTypeEx on ValidateType{
-  String get allowList{
-    switch(this){
-      case ValidateType.EMAIL: {
-        return "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM.@";
-      }
-      case ValidateType.PASSWORD: {
-        return "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
-      }
-      case ValidateType.NUM: {
-        return "1234567890";
-      }
-      case ValidateType.COMMON_STRING: {
-        return "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
-      }
-      default: {
-        return "";
-      }
-    }
+    return false;
   }
 
-  String get title {
-    switch(this){
-      case ValidateType.EMAIL: {
-        return "メールアドレス";
-      }
-      case ValidateType.PASSWORD: {
-        return "パスワード";
-      }
-      case ValidateType.NUM: {
-        return "数値";
-      }
-      case ValidateType.COMMON_STRING: {
-        return "文字列";
-      }
-      default: {
-        return "";
-      }
-    }
-  }
+  return true;
+}
+
+abstract class ValidateType{
+  String get allowListStr;
+  String get allowRange;
+  String get title;
+}
+
+class AddressValidateType extends ValidateType{
+  @override
+  String get allowListStr => "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM.@";
+
+  @override
+  String get allowRange => "半角英数字(大文字、小文字)、@、ドット";
+
+  @override
+  String get title => "メールアドレス";
+}
+
+class PasswordValidateType extends ValidateType{
+  @override
+  String get allowListStr => "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+
+  @override
+  String get allowRange => "半角英数字(大文字、小文字)";
+
+  @override
+  String get title => "パスワード";
+
 }
 
 bool isIncludeKeys(String value, String keys){
   return !keys.split("").any((char) => value.contains(char));
+}
+
+List<String> findInvalidValue(String value, String keys){
+  String invalidValue = value.replaceAll(RegExp('[$keys]'), '');
+  return invalidValue.split("");
+}
+
+class Validate{
+  final String value;
+  final ValidateType validateType;
+
+  Validate({
+    required this.value,
+    required this.validateType,
+  });
 }
