d764212658f9c46ce771268c045ba0c03a4761b8
Rerurate514 <rerurateyuto@gmail.com>
2024-03-29 16:34:39 +0900

    バリデート処理の変更

---
+++ flutter/lib/utils/validate/validate.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:oshikatsu_product/utils/result.dart';
+import 'package:oshikatsu_product/utils/showDialog.dart';
+import 'package:oshikatsu_product/utils/validate/validateType.dart';
+
+Result checkValidate(BuildContext context, Validate target){
+  Result<String> result = _checkValidate(target);
+  if(result.isOk){
+    return Result(isOk: true);
+  }
+  else{
+    showWarnDialog(
+      context, 
+      result.value!
+    );
+    return Result<String>(
+      isOk: false,
+      value: result.value
+    );
+  }
+}
+
+Result<String> _checkValidate(Validate target){
+  Result<String> result = target.errorResult;
+  if(result.isOk){
+    return Result(isOk: true);
+  }
+  else{
+    return Result<String>(
+      isOk: false,
+      value: result.value
+    );
+  }
+}
+
+Result<List<String>> _isNotContainsInvalidValue(Validate target){
+  String invalidValue = target.value.replaceAll(target.validateType.allowRegExp, '');
+  List<String> invalidValueList = invalidValue.split("");
+
+  if(invalidValue.isEmpty){
+    return Result(isOk: true);
+  }
+  else{
+    return Result(
+      isOk: false,
+      value: invalidValueList
+    );
+  }
+}
+
+class Validate{
+  final String value;
+  final ValidateType validateType;
+
+  const Validate({
+    required this.value,
+    required this.validateType,
+  });
+}
+
+extension ValidateErrorMessage on Validate{
+  Result<String> get isNotEnteredValue {
+    if(value != ""){
+      return Result(isOk: true);
+    }
+    else{
+      return Result(
+        isOk: false,
+        value: "${validateType.title}が入力されていません。\n"
+      );
+    }
+  }
+
+  Result<String> get isNotContainsInvalidValue {
+    Result<List<String>> result = _isNotContainsInvalidValue(this);
+    if(result.isOk){
+      return Result(isOk: true);
+    }
+    else{
+      return Result(
+        isOk: false,
+        value: "無効な値 ${result.value} を使用しています。\n使用可能な値は、\n${validateType.letterTypeMessage}です。\n"
+      );
+    }
+  }
+
+  Result<String> get isMatchedAddress {
+    if(validateType is! AddressValidateType) return Result(isOk: true);
+    if(validateType.allowRegExp.hasMatch(value)){
+      return Result(isOk: true);
+    }
+    else{
+      return Result(
+        isOk: false,
+        value: "入力された値がメールアドレスの形式ではありません。\n"
+      );
+    }
+  }
+
+  Result<String> get errorResult {
+    String errorMessage = "";
+
+    Result result = isNotEnteredValue;
+    if(!result.isOk) errorMessage += result.value;
+
+    result = isMatchedAddress;
+    if(!result.isOk) errorMessage += result.value;
+
+    result = validateType.isMatchedAllowRange(this);
+    if(!result.isOk) errorMessage += result.value;
+
+    result = isNotContainsInvalidValue;
+    if(!result.isOk) errorMessage += result.value;
+
+    if(errorMessage == ""){
+      return Result(isOk: true);
+    }
+    else{
+      return Result<String>(
+        isOk: false,
+        value: errorMessage
+      );
+    }
+  }
+}
---
+++ flutter/lib/utils/validate/validateType.dart
@@ -0,0 +1,66 @@
+import 'package:oshikatsu_product/utils/result.dart';
+import 'package:oshikatsu_product/utils/validate/validate.dart';
+
+abstract class ValidateType{
+  RegExp get allowRegExp => RegExp(r'.*');
+  int get allowRangeStart => 0;
+  int get allowRangeEnd => 256;
+  String get letterTypeMessage;
+  String get title;
+
+  Result<String> isMatchedAllowRange(Validate target){
+    int strLen = allowRangeEnd - allowRangeStart;
+    if(target.value.length > strLen){
+      return Result<String>(
+        isOk: false,
+        value: "文字数は$allowRangeEndより少なくしてください"
+      );
+    }
+    else if(target.value.length < strLen){
+      return Result<String>(
+        isOk: false,
+        value: "文字数は$allowRangeStartより多くしてください"
+      );
+    }
+    else{
+      return Result(isOk: true);
+    }
+  }
+}
+
+class AddressValidateType extends ValidateType{
+  @override
+  RegExp get allowRegExp => RegExp('.*@.*');
+
+  @override
+  String get letterTypeMessage => "半角英数字(大文字、小文字)、@、ドット";
+
+  @override
+  String get title => "メールアドレス";
+}
+
+class PasswordValidateType extends ValidateType{
+  @override
+  // ignore: valid_regexps
+  RegExp get allowRegExp => RegExp(r'^[a-zA-Z0-9!"#$%&()*+,-./:;<=>?@[\]^_`{|}~]{8,24}$');
+
+  @override
+  int get allowRangeStart => 8;
+
+  @override
+  int get allowRangeEnd => 24;
+
+  @override
+  String get letterTypeMessage => "半角英数字(大文字、小文字)、記号で$allowRangeStart文字以上$allowRangeEnd文字以下";
+
+  @override
+  String get title => "パスワード";
+}
+
+class StringValidateType extends ValidateType{
+  @override
+  String get letterTypeMessage => "";
+
+  @override
+  String get title => "文字列";
+}
